f24ebad1f1afd75af538e6fa538a766acc6d8718
Shreyas Rajiv <131350902+shreyasrajiv327@users.noreply.github.com>
2023-12-29 16:32:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ st.title("Text Translator")
 language=st.text_input("Enter the Language of Translation")
 text=st.text_input("Enter Text to be Translated")
 
-if language:
+if language and text:
     final=llm.translate(language,text)
     response=final['Translated_text'].strip()
     st.write(response)
